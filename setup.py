ef74952715328cac4255575cee68ae443c0aefc9
Andrew Stanton <astanton@etsy.com>
2018-11-24 14:08:59 -0800

    Version bump

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ BASE = os.path.dirname(os.path.abspath(__file__))
 
 setup(
     name='polymr',
-    version="0.1.1",
-    description="Pure MapReduce implementation in Python",
+    version="0.1.2",
+    description="Data Processing implementation in Python",
     url="https://www.github.com/Refefer/Polymr",
     packages=find_packages(BASE),
     test_suite="tests",
