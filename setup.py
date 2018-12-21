c8a2ca90f2b3144bbaa115d87b44159c321db66c
Andrew Stanton <astanton@etsy.com>
2018-12-20 21:40:33 -0800

    Version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(BASE, "README.md")) as f:
 
 setup(
     name='dampr',
-    version="0.1.4",
+    version="0.1.5",
     description="Data Processing implementation in Python",
     long_description=long_description,
     url="https://www.github.com/Refefer/Dampr",
